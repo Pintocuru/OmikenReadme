4ebcf3f2938d85ecc7dc57046ba29ee0d9b2e3da
Pintocuru <flyiruka@gmail.com>
2025-08-12 12:24:11 +0900

    テンプレート生成コードの開発1

---
+++ src/commonData.ts
@@ -0,0 +1,9 @@
+// src\commonData.ts
+
+export const COMMON_DATA = {
+  APP_AUTHOR: "くるウサ ピント",
+  LICENSE: "MIT License",
+  // 共通の説明文
+  DESC_CONFIG: "アプリの設定ファイルです",
+  DESC_MAIN: "アプリのエントリーポイントです",
+};
